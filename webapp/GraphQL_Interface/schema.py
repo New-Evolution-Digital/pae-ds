7b227202ee5c22ec524f1996abf49df2608ab0d1
peterger8y <peterger8y@gmail.com>
2021-11-28 01:23:15 -0600

    experimenting with schema setup, a ways to go yet

---
+++ webapp/GraphQL_Interface/schema.py
@@ -0,0 +1,58 @@
+import graphene
+import requests
+import os
+
+URL = os.getenv('URL')
+SECRET = os.getenv('SECRET_TOKEN')
+
+
+class vehicleData(graphene.InputObjectType):
+    longitude = graphene.Float()
+    latitude = graphene.Float()
+    year = graphene.Integer()
+    manufacturer = graphene.String()
+    condition = graphene.String()
+    miles = graphene.Integer()
+    type = graphene.String()
+    transmission = graphene.String()
+    drive = graphene.String()
+    color = graphene.String()
+    fuel = graphene.String()
+
+
+class Radius(graphene.ObjectType):
+    radius = graphene.Integer(required=True)
+
+class Option(graphene.ObjectType):
+    option = String()
+
+
+class Query(graphene.ObjectType):
+    # this defines a Field `hello` in our Schema with a single Argument `name`
+    vehicleStats = graphene.Field(vehicle=vehicleData(required=True))
+    vehicleListings = graphene.Field(Option, Radius, vehicle=vehicleData())
+
+    def resolve_vehicleStats(root, info, vehicle):
+        resp = requests.post(URL + '/vehicledata/metrics', auth=SECRET,
+                             data=vehicle)
+        return resp
+
+    def resolve_vehicleListings(root, info, vehicle):
+        resp = requests.post(URL + 'vehicledata/listings', auth=SECRET,
+                             data=vehicle)
+        return resp
+
+
+schema = graphene.Schema(query=Query)
+
+# we can query for our field (with the default argument)
+query_string = '{ goodbye }'
+result = schema.execute(query_string)
+print(result.data['goodbye'])
+# "Hello stranger!"
+
+# or passing the argument in the query
+query_with_argument = '{ hello(name: "GraphQL") }'
+result = schema.execute(query_with_argument)
+print(result.data['hello'])
+# "Hello GraphQL!"
